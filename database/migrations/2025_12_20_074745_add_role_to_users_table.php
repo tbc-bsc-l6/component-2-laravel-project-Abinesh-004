108b869f9f09433433c18cfc27686f65ee2480fb
Abinesh_Acharya <121370307+Mountainwitch1@users.noreply.github.com>
2025-12-20 13:38:13 +0545

    added student as a default user for authentication
    admin cannot be created from self-register

--- database/migrations/2025_12_20_074745_add_role_to_users_table.php
+++ database/migrations/2025_12_20_074745_add_role_to_users_table.php
@@ -11,8 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
+        // Schema::table('users', function (Blueprint $table) {
+            
+        // });
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('role')->default('student');
         });
     }
 
