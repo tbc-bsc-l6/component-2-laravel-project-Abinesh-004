79405f8f700f3cd1489f18d61b47219398194d26
Abinesh_Acharya <121370307+Mountainwitch1@users.noreply.github.com>
2026-01-10 19:22:09 +0545

    updated password requirement

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -32,7 +32,7 @@ class RegisteredUserController extends Controller
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'password' => ['required', 'confirmed', Rules\Password::min(8)->mixedCase()->letters()->numbers()->symbols()],
         ]);
 
         // Get the student role ID (default role for new registrations)
